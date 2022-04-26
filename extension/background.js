fc928994c7f67015fd98ee01cd5c92de21759fd7
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2022-04-26 19:45:11 +0530

    Generate CSS Path for clicked element

--- extension/background.js
+++ extension/background.js
@@ -1,29 +1,30 @@
 const grabBtn = document.querySelector(".grab-btn");
 
-function receiveItem(message) {
-  const a = message.item;
-  console.log(a);
-}
+// function receiveItem(message) {
+//   const a = message.item;
+//   console.log(a);
+// }
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tab = tabs[0];
+  const currentUrl = tab.url;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  if (request.greeting == "hello") {
-    sendResponse({ farewell: "goodbye" });
-    return true;
-  }
-});
-
-grabBtn.addEventListener("click", () => {
-  console.log("btn clicked");
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tab = tabs[0];
+  grabBtn.addEventListener("click", () => {
+    console.log("btn clicked");
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["content-script.js"],
     });
   });
 });
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   console.log(
+//     sender.tab
+//       ? "from a content script:" + sender.tab.url
+//       : "from the extension"
+//   );
+//   if (request.greeting == "hello") {
+//     sendResponse({ farewell: "goodbye" });
+//     return true;
+//   }
+// });
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,20 +1,34 @@
-function passItem() {
-  chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
-    console.log(response.farewell);
-  });
-}
-
-// function generateCSSTree(item) {
-//   if (item.target.parentNode === "li") {
-//     console.log(item.target.parentNode);
-//   } else {
-//     console.log(item.target.parentNode);
-//   }
+// function passItem() {
+//   chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
+//     console.log(response.farewell);
+//   });
 // }
 
+// #js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-sidebar > div > div.js-profile-editable-replace > div.d-flex.flex-column > div.js-profile-editable-area.d-flex.flex-column.d-md-block > div.flex-order-1.flex-md-order-none.mt-2.mt-md-0 > div > a:nth-child(1) > span
+
+function generateCSSPath(item) {
+  const newPath = item.path.reverse();
+  console.log(newPath);
+  let cssPath = "";
+  for (let i = 0; i < newPath.length; i++) {
+    if (newPath[i].localName === undefined) {
+      console.log(newPath[i]);
+    } else {
+      if (i === newPath.length - 1) {
+        cssPath += `${newPath[i].localName}`;
+      } else {
+        cssPath += `${newPath[i].localName} > `;
+      }
+    }
+  }
+  return cssPath;
+}
+
 document.body.addEventListener("click", (e) => {
+  e.preventDefault();
   const clickedElement = e;
   console.log(clickedElement);
-  passItem();
-  //   generateCSSTree(clickedElement);
+  // passItem();
+  const path = generateCSSPath(clickedElement);
+  console.log(path);
 });
