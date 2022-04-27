c80979a4b932d447ac20cca09971bfa684c215b4
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2022-04-28 01:47:00 +0530

    create JSON object for grabbed element

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,20 @@
-const grabBtn = document.querySelector(".grab-btn");
-
-function handleMessage(request, sender, sendResponse) {
-  console.log(`content script sent a message: ${request.content}`);
-}
-
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const tab = tabs[0];
-  const currentUrl = tab.url;
-
-  grabBtn.addEventListener("click", () => {
-    console.log("btn clicked");
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["content-script.js"],
-    });
-    chrome.runtime.onMessage.addListener(handleMessage);
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: 'Grab this element"',
+    contexts: ["all"],
+    id: "myContextMenuId",
   });
 });
+
+//send message to content script
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  chrome.tabs.sendMessage(tab.id, { message: "grab item" });
+});
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === "get url") {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//       sendResponse({ url: tabs[0].url });
+//     });
+//   }
+// });
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,10 +1,10 @@
-function passElement() {
-  chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
-    console.log(response.farewell);
-  });
-}
+// function passElement() {
+//   chrome.runtime.sendMessage({ content: "hello" });
+//   console.log("passed element");
+// }
+console.log("loaded");
 
-function generateCSSPath(ele) {
+const generateCSSPath = (ele) => {
   const newPath = ele.path.reverse();
   console.log(newPath);
   let cssPath = "";
@@ -18,13 +18,78 @@ function generateCSSPath(ele) {
     }
   }
   return cssPath;
-}
+};
 
-document.body.addEventListener("click", (e) => {
-  e.preventDefault();
-  const clickedElement = e;
-  console.log(clickedElement);
-  const path = generateCSSPath(clickedElement);
-  console.log(path);
-  passElement();
+const copyStringToClipboard = (str) => {
+  var el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style = { position: "absolute", left: "-9999px" };
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+const generateObject = (element, event) => {
+  const currentUrl = window.location.href;
+  const cssPath = generateCSSPath(event);
+  const elementData = element.innerText;
+  const elementObject = {
+    url: currentUrl,
+    path: cssPath,
+    data: elementData,
+  };
+  const elementString = JSON.stringify(elementObject);
+  copyStringToClipboard(elementString);
+  console.log(elementString);
+};
+
+const createButtonGroup = (element, event) => {
+  const buttonGroupContainer = document.createElement("div");
+  const cancelButton = document.createElement("button");
+  const grabButton = document.createElement("button");
+  const selectParentButton = document.createElement("button");
+
+  cancelButton.innerText = "Cancel";
+  grabButton.innerText = "Grab";
+  selectParentButton.innerText = "Select Parent";
+
+  buttonGroupContainer.appendChild(cancelButton);
+  buttonGroupContainer.appendChild(grabButton);
+  buttonGroupContainer.appendChild(selectParentButton);
+
+  cancelButton.addEventListener("click", () => {
+    element.style.border = "";
+    buttonGroupContainer.remove();
+  });
+
+  grabButton.addEventListener("click", () => {
+    generateObject(element, event);
+    element.style.border = "";
+    buttonGroupContainer.remove();
+  });
+
+  return buttonGroupContainer;
+};
+
+document.body.addEventListener("contextmenu", (e) => {
+  const menuEvent = e;
+  const ele = menuEvent.target;
+  console.log(ele);
+  chrome.runtime.onMessage.addListener(function (request) {
+    if (request.message === "grab item") {
+      ele.style.border = "2px solid blue";
+      const buttonGroup = createButtonGroup(ele, menuEvent);
+      ele.parentNode.insertBefore(buttonGroup, ele.nextSibling);
+    }
+  });
+
+  //   console.log(ele);
+  //   //   console.log(e);
+  //   //   // const clickedElement = e;
+  //   //   // console.log(clickedElement);
+  //   //   // const path = generateCSSPath(clickedElement);
+  //   //   // console.log(path);
+  //   //   // passElement();
 });
